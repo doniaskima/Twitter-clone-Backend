11ff7744ca903afbb3a23612754614886a99bc02
unknown <doniaskima344@gmail.com>
2022-10-04 00:04:20 +0100

    *dislikePost*(#gonna slice the uderId instead push)

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -40,5 +40,25 @@ const likePost = async(req, res) => {
 }
 
 
+const dislikePost = async(req, res) => {
+    const { userId, postId } = req.body;
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.json({ success: false, massage: "User not found" });
+        }
+        const post = await Post.findById(postId);
+        if (!post) {
+            return res.json({ success: false, massage: "User not found" });
+        }
+        post.likes.slice(user._id);
+        await post.save();
+        return res.status(200).json({ success: true, message: "post disliked" });
+    } catch (error) {
+        return res.status(500).json({ success: false, message: error.message });
+    }
+};
 
-module.exports = { createPost, likePost };
+
+
+module.exports = { createPost, likePost, dislikePost };
