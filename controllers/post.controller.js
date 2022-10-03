cbf685b9feb77228f9713c0541b7fd05fe93cac1
unknown <doniaskima344@gmail.com>
2022-10-04 00:18:52 +0100

    *commentPost*

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,5 +1,6 @@
 const { User } = require("../models/user.model");
 const { Post } = require("../models/post.model");
+const { Comment } = require("../models/comment.model");
 
 
 const createPost = async(req, res) => {
@@ -60,7 +61,29 @@ const dislikePost = async(req, res) => {
 };
 
 const commentPost = async(req, res) => {
-
+    const { userId, postId, comment } = req.body;
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.json({ success: false, message: "User not found" })
+        }
+        const post = await Post.findById(postId);
+        if (!post) {
+            return res.json({ success: false, massage: "User not found" });
+        }
+        const newComment = new Comment({
+            comment: comment,
+            commentBy: user._id,
+            postId: post._id,
+        });
+        await newComment.save();
+        return res.status(200).json({
+            success: true,
+            message: "comment added"
+        });
+    } catch (error) {
+        return res.status(500).json({ success: true, message: error.message });
+    }
 }
 
 
