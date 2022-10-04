dcf3245c49ca65e3709e62747d96a9de0369c230
unknown <doniaskima344@gmail.com>
2022-10-04 19:03:05 +0100

    correct the notification model

--- models/notification.model.js
+++ models/notification.model.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const notificationSchema = new mongoose.Schema({
-    type: { type: String, enum: ["NEW_FOLLOWER", "LIKED", "NEW_COMMENT"] },
+    type: {
+        type: String,
+        enum: ["NEW_FOLLOWER", "LIKED", "NEW_COMMENT"],
+    },
+    postId: {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+    },
     targetId: { type: Schema.Types.ObjectId, ref: "User" },
-    postId: { type: Schema.Types.ObjectId, ref: "User" },
     sourceId: { type: Schema.Types.ObjectId, ref: "User" },
-
+    isRead: { type: Boolean, default: false },
 }, { timestamps: true });
 
 const Notification = mongoose.model("notifications", notificationSchema);
