dfac5f3b44771eedbedea8b3487338643a4fc317
unknown <doniaskima344@gmail.com>
2022-10-05 18:05:50 +0100

    more rotes added

--- routers/post.router.js
+++ routers/post.router.js
@@ -4,10 +4,16 @@ const {
     createPost,
     likePost,
     commentPost,
-    dislikePost,
+    unlikePost,
+    fetchLikes,
+    deletePost,
+    fetchComments,
 } = require("../controllers/post.controller");
 
 router.route("/new").post(createPost);
 router.route("/like").post(likePost);
 router.route("/comment").post(commentPost);
-router.route("/dislike").post(dislikePost);
+router.route("/dislike").post(unlikePost);
+router.route("/likes/:postId").get(fetchLikes);
+router.route("/comments/postId").get(fetchComments);
+router.route("delete/:postId").delete(deletePost)
