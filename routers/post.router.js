946700ae05d662b502828a5e3fb909b929b5ae8c
unknown <doniaskima344@gmail.com>
2022-10-07 12:20:42 +0100

    new routes

--- routers/post.router.js
+++ routers/post.router.js
@@ -8,12 +8,18 @@ const {
     fetchLikes,
     deletePost,
     fetchComments,
+    deleteComment,
+    fetchSinglePost,
+    updatePost,
 } = require("../controllers/post.controller");
 
 router.route("/new").post(createPost);
 router.route("/like").post(likePost);
 router.route("/comment").post(commentPost);
+router.route("/comment/:commentId").delete(deleteComment);
+router.route("/:postId").get(fetchSinglePost);
 router.route("/dislike").post(unlikePost);
 router.route("/likes/:postId").get(fetchLikes);
 router.route("/comments/postId").get(fetchComments);
+router.route("/update-post").put(updatePost);
 router.route("delete/:postId").delete(deletePost)
