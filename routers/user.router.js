37f164e062876b63e84510c8f5735a31bd5186da
unknown <doniaskima344@gmail.com>
2022-10-12 01:41:55 +0100

    authenticate middleware in user.router

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const authenticate = require("../middleware/authenticate");
 const router = express.Router();
+
 const {
     fetchUserNotifications,
 } = require("../controllers/notification.controller");
@@ -9,20 +11,30 @@ const {
     signup,
     updateCurrentUserDetails,
     fetchUserPosts,
-    searchUser,
-    getUserFeed,
     follow,
+    unFollow,
+    getUserFeed,
+    fetchUserFollowers,
+    fetchUserFollowing,
+    getSingleUserInfo,
+    searchUser,
     getUserChats,
 } = require("../controllers/user.controllers");
 
 router.route("/login").post(login);
 router.route("/signup").post(signup);
-router.route("/follow").post(follow);
+router.route("/follow").post(authenticate, follow);
+router.route("/unfollow").post(authenticate, unFollow);
+router.route("/search").get(authenticate, searchUser);
+
 router.param("userId", searchById);
-router.route("/chats/:userId", getUserChats)
-router.route("/get-user-posts/:userId").get(fetchUserPosts);
-router.route("/update/:userId").put(updateCurrentUserDetails);
-router.route("/notifications/:userId").get(fetchUserNotifications);
-router.route("/search").get(searchUser);
-router.route("/feed/:userId").get(getUserFeed);
+router.route("/:userId").get(authenticate, getSingleUserInfo);
+router.route("/chats/:userId").get(authenticate, getUserChats);
+router.route("/feed/:userId").get(authenticate, getUserFeed);
+router.route("/followers").post(authenticate, fetchUserFollowers);
+router.route("/following").post(authenticate, fetchUserFollowing);
+router.route("/get-user-posts").post(authenticate, fetchUserPosts);
+router.route("/update/:userId").put(authenticate, updateCurrentUserDetails);
+
+
 module.exports = router;
