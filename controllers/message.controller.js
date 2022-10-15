336c39395913f8412f31b40fddf26f86a8c873a9
unknown <doniaskima344@gmail.com>
2022-10-15 18:52:52 +0100

    encrypt funct copied

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -1,1 +1,22 @@
- 
+const Message = require("../models/message.model");
+const User = require("../models/user.model");
+const crypto = require("crypto");
+
+
+
+const encrypt = (message) => {
+    // key to encrypt and decrypted  (random 32 Bytes)
+    const key = crypto.randomBytes(32);
+    //iv - initialization vector (random 16 Bytes)
+    const iv = crypto.randomBytes(16);
+    // cipher function to encrypt the message
+    // aes-256-cbc algorithm to encrypt and decrypt the data.
+    let cipher = crypto.createCipheriv("aes-256-cbc", Buffer.from(key), iv);
+    let encryptedMessage = cipher.update(message);
+    encryptedMessage = Buffer.concat([encryptedMessage, cipher.final()]);
+    return {
+        iv: iv.toString("hex"),
+        encryptedMessage: encryptedMessage.toString("hex"),
+        key: key.toString("hex"),
+    };
+};
