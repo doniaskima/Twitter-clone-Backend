57dc044ccbc143eaa8c17dcbc4cacb82ac623df2
unknown <doniaskima344@gmail.com>
2022-10-16 02:45:31 +0100

    unbookmarkPost added

--- controllers/bookmark.controller.js
+++ controllers/bookmark.controller.js
@@ -5,12 +5,12 @@ const mongoose = require("mongoose");
 const bookmarkPost = async(req, res) => {
     try {
         let isPostBookmarked = null;
-        isStoryBookmarked = await bookmarkModels.findOne({
+        isPostBookmarked = await bookmarkModels.findOne({
             post: req.post._id,
             user: req.verifiedUser._id,
         });
-        if (!isStoryBookmarked) {
-            isStoryBookmarked = new bookmarkModels({
+        if (!isPostBookmarked) {
+            isPostBookmarked = new bookmarkModels({
                 user: req.verifiedUser._id,
                 post: req.post._id,
             });
@@ -23,7 +23,18 @@ const bookmarkPost = async(req, res) => {
 }
 
 
+const unbookmarkPost = async(req, res) => {
+    try {
+        await bookmarkModels.deleteOne({
+            user: req.verifiedUser._id,
+            post: req.post._id,
+        });
+        return res.status(204).json();
 
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+}
 
 
 
@@ -31,3 +42,4 @@ const bookmarkPost = async(req, res) => {
 
 
 module.exports.bookmarkPost = bookmarkPost;
+module.exports.unbookmarkPost = unbookmarkPost;
