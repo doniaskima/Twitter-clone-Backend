cf78bb18460e555aa6433df8288911bd59a410c0
unknown <doniaskima344@gmail.com>
2022-10-18 21:18:30 +0100

    fixing problems

--- models/bookmark.models.js
+++ models/bookmark.models.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const { Schema } = mongoose;
 
 const BookmarkSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
--- models/message.model.js
+++ models/message.model.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const { Schema } = mongoose;
 const messageSchema = new mongoose.Schema({
     sender: {
         type: mongoose.Schema.Types.ObjectId,
