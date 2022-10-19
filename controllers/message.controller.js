c209aa9b61a407494a2be4f2fb17891d1765bf7c
unknown <doniaskima344@gmail.com>
2022-10-19 09:11:02 +0100

    fixing the message controller

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -18,29 +18,6 @@ const encrypt = (message) => {
         key: key.toString("hex"),
     };
 };
-const startMessage = async(senderId, receiverEmail) => {
-    const user = await User.findOne({ _id: senderId });
-    if (user) {
-        const receiver = await User.findOne({ email: receiverEmail });
-        if (receiver) {
-            if (!user.chats.includes(senderId) && user._id !== receiver._id) {
-                user.chats.push(receiver._id);
-                await user
-                    .save()
-                    .then(() => {
-                        return true;
-                    })
-                    .catch(() => {
-                        return null;
-                    });
-            }
-        } else {
-            return null;
-        }
-    } else {
-        return null;
-    }
-};
 
 const createMessage = async(senderId, receiverEmail, message) => {
     let info = null;
@@ -48,7 +25,6 @@ const createMessage = async(senderId, receiverEmail, message) => {
     const user = await User.findOne({ _id: senderId }).catch((err) => {
         console.log(err);
     });
-
     if (user) {
         const receiver = await User.findOne({ email: receiverEmail });
         if (receiver) {
@@ -78,30 +54,140 @@ const createMessage = async(senderId, receiverEmail, message) => {
                     name: receiver.name,
                     _id: receiver._id,
                     email: receiver.email,
-                    profileUrl: user.profileUrl,
-                    username: user.username,
+                    profileUrl: receiver.profileUrl,
+                    username: receiver.username,
                 },
                 iv: newMessage.iv,
                 key: newMessage.key,
-                message: newMessage.key,
-                createdAt: newMessage.createdAt,
+                message: newMessage.message,
+                createdAt: message.createdAt,
                 messageId: newMessage._id,
-
-            }
-
+            };
         }
     }
     return { info, isNewRecipient };
 };
 
-const deleteMessageById = async(req, res) => {
-    const { messageId } = req.params;
-    Message.findByIdAndDelete(messageId).then(() => {
-        return res.json({ success: true, message: "message deleted" });
-    }).catch((err) => {
-        console.log(err);
-        return res.json({ success: false, message: err.message });
+const startMessage = async(senderId, receiverEmail) => {
+    const user = await User.findOne({ _id: senderId });
+    if (user) {
+        const receiver = await User.findOne({ email: receiverEmail });
+        if (receiver) {
+            if (!user.chats.includes(senderId) && user._id !== receiver._id) {
+                user.chats.push(receiver._id);
+                await user
+                    .save()
+                    .then(() => {
+                        return true;
+                    })
+                    .catch(() => {
+                        return null;
+                    });
+            }
+        } else {
+            return null;
+        }
+    } else {
+        return null;
+    }
+};
+
+const getMessages = (req, res) => {
+    const { userId, receiverId } = req.body;
+    User.findOne({ _id: userId }, (err, user) => {
+        if (err) {
+            return res.status(500).json({ success: false, message: err.message });
+        } else if (!user) {
+            return res.json({ success: false, message: "user not exist" });
+        } else {
+            User.findOne({ _id: receiverId }, (err, receiver) => {
+                if (err) {
+                    return res.status(500).json({ success: false, message: err.message });
+                } else if (!receiver) {
+                    return res
+                        .status()
+                        .json({ success: false, message: "receiver not exist" });
+                } else {
+                    Message.find({ sender: userId, receiver: receiverId })
+                        .then((messagesSentBySender) => {
+                            Message.find({ sender: receiverId, receiver: userId },
+                                (err, messagesSentByReceiver) => {
+                                    let conversation = messagesSentBySender.concat(
+                                        messagesSentByReceiver
+                                    );
+                                    conversation.sort((a, b) => {
+                                        return new Date(a.createdAt) - new Date(b.createdAt);
+                                    });
+                                    let result = [];
+                                    conversation.forEach((message) => {
+                                        let info;
+                                        if (String(message.sender) === String(userId)) {
+                                            info = {
+                                                sender: {
+                                                    name: user.name,
+                                                    email: user.email,
+                                                    _id: user._id,
+                                                },
+                                                receiver: {
+                                                    name: receiver.name,
+                                                    email: receiver.email,
+                                                    _id: receiver._id,
+                                                },
+                                                iv: message.iv,
+                                                key: message.key,
+                                                message: message.message,
+                                                createdAt: message.createdAt,
+                                                messageId: message._id,
+                                            };
+                                        } else {
+                                            info = {
+                                                receiver: {
+                                                    name: user.name,
+                                                    email: user.email,
+                                                    _id: user._id,
+                                                },
+                                                sender: {
+                                                    name: receiver.name,
+                                                    email: receiver.email,
+                                                    _id: receiver._id,
+                                                },
+                                                iv: message.iv,
+                                                key: message.key,
+                                                createdAt: message.createdAt,
+                                                message: message.message,
+                                                messageId: message._id,
+                                            };
+                                        }
+                                        result.push(info);
+                                    });
+                                    return res.json({ success: true, messages: result });
+                                }
+                            );
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                            return res.json({ success: false, message: err.message });
+                        });
+                }
+            });
+        }
     });
+};
+
+const deleteMessageById = (req, res) => {
+    const { messageId } = req.params;
+    Message.findByIdAndDelete(messageId)
+        .then(() => {
+            return res.json({
+                success: true,
+                message: "message deleted",
+                messageId: messageId,
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            return res.json({ success: false, message: err.message });
+        });
 };
 
 const deleteMessages = (senderId, receiverId) => {
@@ -125,7 +211,6 @@ const deleteMessages = (senderId, receiverId) => {
         });
 };
 
-
 const deleteChatByRecipientId = async(req, res) => {
     const { senderId, recipientId } = req.body;
 
@@ -145,7 +230,7 @@ const deleteChatByRecipientId = async(req, res) => {
 };
 
 module.exports = {
-
+    getMessages,
     createMessage,
     deleteMessageById,
     startMessage,
