62e193640e5504c1835d564a2f039466cd8124f6
unknown <doniaskima344@gmail.com>
2023-01-19 22:37:32 +0100

    refactor

--- models/user.model.js
+++ models/user.model.js
@@ -1,39 +1,44 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
+const express = require("express");
+const authenticate = require("../middleware/authenticate");
+const router = express.Router();
 
-const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    username: {
-        type: String,
-        trim: true,
-        unique: true,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: true,
-    },
-    followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
-    following: [{ type: Schema.Types.ObjectId, ref: "User" }],
-    bio: {
-        type: String,
-    },
-    profileUrl: {
-        type: String,
-    },
-    isEmailVerified: { type: Boolean, default: false },
-    chats: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-}, { timestamps: true });
+const {
+    fetchUserNotifications,
+} = require("../controllers/notification.controller");
+const {
+    login,
+    searchById,
+    signup,
+    updateCurrentUserDetails,
+    fetchUserPosts,
+    follow,
+    unFollow,
+    getUserFeed,
+    fetchUserFollowers,
+    fetchUserFollowing,
+    getSingleUserInfo,
+    fetchRecentlyJoinedUsers,
+    searchUser,
+    getUserChats,
+} = require("../controllers/user.controllers");
+router.route("/login").post(login);
+router.route("/signup").post(signup);
+router.route("/follow").post(authenticate, follow);
+router.route("/unfollow").post(authenticate, unFollow);
+router.route("/search").get(authenticate, searchUser);
+router.param("userId", searchById);
+router.route("/:userId").get(authenticate, getSingleUserInfo);
+router.route("/chats/:userId").get(authenticate, getUserChats);
+router.route("/feed/:userId").get(authenticate, getUserFeed);
+router.route("/followers").post(authenticate, fetchUserFollowers);
+router.route("/following").post(authenticate, fetchUserFollowing);
+router.route("/get-user-posts").post(authenticate, fetchUserPosts);
+router.route("/update/:userId").put(authenticate, updateCurrentUserDetails);
+router
+    .route("/notifications/:userId")
+    .get(authenticate, fetchUserNotifications);
+router
+    .route("/get-recently-joined-users/:userId")
+    .get(fetchRecentlyJoinedUsers);
 
-const User = mongoose.model("users", userSchema);
-
-module.exports = User;
+module.exports = router;
